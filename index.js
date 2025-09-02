149d60d47405e24e12d729aba4b843c473dd7e16
huevangxp <huevang933@gmail.com>
2025-09-02 08:58:10 +0700

    commit Tue 09/02/2025  8:58:10.21

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ function getProducts() {
             products = data.data;
             console.log(products)
             // const products = data;
-            displayProducts(products);
+            // get product class to map products
+            const productClass = document.querySelector('.products');
+            productClass.innerHTML = '';
+            products.forEach(product => {
+                const productElement = document.createElement('div');
+                productElement.classList.add('product');
+                productElement.innerHTML = `
+                    <img src="${product.image}" alt="Product Image">
+                    <h3>${product.name}</h3>
+                    <p>${product.description}</p>
+                    <p>Price: $${product.price}</p>
+                `;
+                productClass.appendChild(productElement);
+            });
         })
         .catch(error => console.error('Error fetching products:', error));
 }
 
 getProducts();
 
-function displayProducts(products) {
-    const productsContainer = document.querySelector('.products');
-    products.forEach(product => {
-        const productElement = document.createElement('div');
-        productElement.classList.add('product');
-        productElement.innerHTML = `
-            <img src="${product.image}" alt="Product Image">
-            <h3>${product.name}</h3>
-            <p>${product.description}</p>
-            <p>Price: $${product.price}</p>
-        `;
-        productsContainer.appendChild(productElement);
-    });
-}
+ 
